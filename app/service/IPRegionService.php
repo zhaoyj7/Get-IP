3df8971e0d3886444f997084a7bd2a36a10b00d2
zhaoyj <admin@silveridc.cn>
2026-06-13 02:07:40 +0800

    fix

--- app/service/IPRegionService.php
+++ app/service/IPRegionService.php
@@ -1,5 +1,5 @@
 <?php
-namespace app\service;
+namespace app\api\services;
 
 class IPRegionService
 {
@@ -14,16 +14,16 @@ class IPRegionService
      * @return string Province - 省
      * @return string City - 市
      */
-    public function GetInfo($ipAddr)
+    public function GetInfo(string $ipAddr)
     {
-        $IP2Region = new \Ip2Region('file');
-        $IPInfo = $IP2Region->getIpInfo($ipAddr);
-        $Country = $IPInfo['country'];
-        $Province = $IPInfo['province'];
-        $City = $IPInfo['city'];
+        $IP2Region = (new \Ip2Region('file', root_path().'extend/ip2region_v4.xdb', root_path().'extend/ip2region_v6.xdb'))->getIpInfo($ipAddr);
+        $Country = $IP2Region['country'];
+        $Province = $IP2Region['province'];
+        $City = $IP2Region['city'];
+        $ISP = $IP2Region['isp'];
         if (in_array("Reserved", [$Country, $Province, $City])) {
             $result = "PrivateIP";
-        } else $result = ['Country'=>$Country,'Province'=>$Province,'City'=>$City];
+        } else $result = ['Country'=>$Country,'Province'=>$Province,'City'=>$City,'ISP'=>$ISP];
         return $result;
     }
 }
